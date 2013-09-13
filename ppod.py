d5c0966c846a891ced65ecd23061d5da942332f8
Chris Martel <chris@codeways.org>
2013-09-13 15:08:17 +0200

    Fixed typos in module docstring.  Added readme.

--- ppod.py
+++ ppod.py
@@ -20,24 +20,27 @@
 :mod:`ppod` -- Download podcasts
 ================================
 
-.. module:: ppd
+.. module:: ppod
    :platform: Unix
    :synopsis: Download podcasts episodes.
 .. moduleauthor:: Christoph Martel <chris@codeways.org>
 
-Ppod reads feed information from an opml file called `subscriptions.opml`.
+Ppod reads feed information from an opml file called ``subscriptions.opml``.
 It downloads the latest episode of each podcast defined in that file.  Ppod
 renames the downloaded audio files using their publication date and updates
 their mp3 tag information.  Download progress is displayed in a curses window.
 
 Use this program at your own risk!
 
-Requirements:
+Requirements::
+
     feedparser
     eyed3
     dateutil
 
-Usage: ./ppod.py
+Usage::
+
+    ./ppod.py
 '''
 
 import xml.etree.ElementTree as ET
